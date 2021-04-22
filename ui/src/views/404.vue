1705041a13cbaab5e519fba9a73c495d229fa15f
harveybland <harvey.bland@genius.online>
2021-04-22 10:08:49 +0100

    Latest build

---
+++ ui/src/views/404.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>404 error page
+    </div>
+</template>
+
+<script>
+    export default {
+        name: '404'
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ ui/src/views/Search.vue
@@ -0,0 +1,82 @@
+<template>
+    <div>
+        <div class="header mb-4">
+        <div class="routes">
+            <router-link to="/Vacancies">View</router-link>
+            <router-link to="/NewVacancy">Create</router-link>
+            <router-link to="/search">Search</router-link>
+        </div>
+        </div>
+        <div class="container account mt-5 box">
+        <div class="title">
+            <h4 class="pt-3 pb-3 mb-0">Search Page</h4>
+        </div>
+      <b-form class="search mt-4 ml-5 mb-3 mr-5">
+        <div class="flexL">
+            <b-form-group>
+                <label>Vacancy Title</label>
+              <b-form-input placeholder="Developer" class="form-control"></b-form-input>
+          </b-form-group>
+
+          <b-form-group>
+                <label>Vacancy Id</label>
+              <b-form-input placeholder="123" type="password"  class="form-control"></b-form-input>
+          </b-form-group>
+
+            <b-form-group>
+                <label>Location</label>
+              <b-form-input placeholder="Locations" class="form-control"></b-form-input>
+          </b-form-group>
+        </div>
+        <div class="flexR">
+          <b-form-group>
+                <label>Recruitment User</label>
+              <b-form-input placeholder="Recruitment User" type="password"  class="form-control"></b-form-input>
+          </b-form-group>
+
+            <b-form-group>
+                <label>Line manager</label>
+              <b-form-input placeholder="admin" class="form-control"></b-form-input>
+          </b-form-group>
+
+          <b-form-group>
+                <label>Vacancy Status</label>
+              <b-form-input placeholder="Status" type="password"  class="form-control"></b-form-input>
+          </b-form-group>
+        </div>
+      </b-form>
+        <b-button class="mb-4" type="submitted" @click.prevent="search">Search</b-button>
+    </div>
+    </div>
+</template>
+
+<script>
+import { gsap } from "gsap";
+export default {
+        name: 'Search',
+      mounted() {
+        gsap.from(".box", {
+        duration: 0.8,
+        y: 200,
+        opacity: 0, 
+      })
+    }
+    }
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/styles.scss'; 
+  .search {
+    display: flex;
+    .flexL {
+    width: 50%;
+    margin-right: 50px;
+  }
+    .flexR {
+    width: 50%;
+  }
+}
+label {
+  display: flex;
+}
+</style>
